c202020168068705bcc81fce8a689850cad6fa15
Michael Moser <mmoser@redhat.com>
2020-02-18 12:34:25 +0200

    typos in the script
    Signed-off-by: Michael Moser <mmoser@redhat.com>

--- check-image-listed.sh
+++ check-image-listed.sh
@@ -1,12 +1,23 @@
 #!/bin/bash -e
 
+
 # this script (if placed into the dev-script directory) checks if the current version defined in OPENSHIFT_RELEASE_IMAGE is listed as a 'green' image 
 # crawls https://openshift-release.svc.ci.openshift.org/ and extracts 'green links'
 
 . config_${USER}.sh
 
+URL="https://openshift-release.svc.ci.openshift.org/"
+
 function download_version_page { 
-    wget https://openshift-release.svc.ci.openshift.org/ -O out.html                                                                                                                          
+    set +e
+    echo "downloading $URL ... "
+    wget "$URL" -O out.html
+    STAT=$?
+    set -e
+    if [[ "$STAT" != "0" ]]; then
+      echo "download $URL failed"
+      exit 1
+    fi
 }
 
 function get_green_versions {
@@ -20,6 +31,8 @@ if [[ "$OPENSHIFT_RELEASE_IMAGE" == "" ]]; then
 fi
 
 
+set +x
+
 VER=$(echo $OPENSHIFT_RELEASE_IMAGE | sed -n 's/registry.svc.ci.openshift.org\/ocp\/release:\(.*\)$/\1/p')
 
 if [[ "$VER" == "" ]]; then
@@ -29,7 +42,7 @@ fi
 
 download_version_page
 
-IS_PRESENT=$(get_green_versions | grep "$VERSION" | wc -l)
+IS_PRESENT=$(get_green_versions | grep -F "$VER" | wc -l)
 
 if [ "$IS_PRESENT" == "0" ]; then
   echo "version $VER not listed as green version (OPENSHIFT_RELEASE_IMAGE: $OPENSHIFT_RELEASE_IMAGE)"
